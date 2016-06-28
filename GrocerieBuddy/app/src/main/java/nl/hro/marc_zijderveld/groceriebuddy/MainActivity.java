30d42d4c10b10bd77ece4c6b7451faec19357263
Sleepyz4life <mwz_23@msn.com>
2016-06-28 13:52:38 +0200

    Initial commit

---
+++ GrocerieBuddy/app/src/main/java/nl/hro/marc_zijderveld/groceriebuddy/MainActivity.java
@@ -0,0 +1,13 @@
+package nl.hro.marc_zijderveld.groceriebuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
